561e6612e864268fc7ab492508648c0f464fdd9c
Lucas Marques <luquinhas.mb07@gmail.com>
2021-08-17 14:31:37 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Tasks from './components/Tasks';
 import './App.css';
 
 const App = () => {
-  const messagem = "hello world!";
-  return <div className="container"></div>
-}
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    }
+  ]);
+  return(
+    <>
+    <div className="container">
+      <Tasks />
+    </div>
+    </>
+  ); 
+};
 export default App;
